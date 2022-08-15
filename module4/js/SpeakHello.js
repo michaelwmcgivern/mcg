9cbd4d962ec16ce4993e085d0c95137d30626df2
michaelwmcgivern <michaelwmcgivern@hotmail.com>
2022-08-14 20:29:55 -0400

    module3

--- module4/js/SpeakHello.js
+++ module4/js/SpeakHello.js
@@ -7,3 +7,14 @@
     }
         window.helloSpeaker = helloSpeaker;
     })(window);
+
+    (function (window){
+    var byeSpeaker = {};
+    var speakWord = "Good Bye";
+
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+
+     window.byeSpeaker = byeSpeaker;
+    })(window);
